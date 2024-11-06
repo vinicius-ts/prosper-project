b9cb5f6604a96144f82145823f8c1e262bcffed5
vinicius-ts <vinicius.silva@omie.com.br>
2024-11-06 19:02:26 -0300

    feat: remove drop

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from core.database import Base, engine
 
 async def init_models():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 async def startup_event():
